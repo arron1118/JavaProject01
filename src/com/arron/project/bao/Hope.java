3d676678c5d8439f56ccee15e50eb3b5da1b6f25
arron1118 <arron1118@aliyun.com>
2018-02-02 10:31:24 +0800

    Hope.java
    add new file Hope.java

---
+++ src/com/arron/project/bao/Hope.java
@@ -0,0 +1,13 @@
+package com.arron.project.bao;
+
+public class Hope {
+  final String NAME = "HOPE";
+  
+  public static void main(String[] args) {
+    getName();
+  }
+  
+  public static void getName() {
+    System.out.println(NAME);
+  }
+}
